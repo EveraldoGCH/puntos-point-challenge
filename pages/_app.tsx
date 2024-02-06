5ea7bbdf5918fa42e2cd61393477987cca5c46af
EveraldoGCH <everaldoguevara@gmail.com>
2024-02-05 22:14:43 -0300

    gtm script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import '@fontsource/roboto/700.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
+import Script from 'next/script';
 
 const queryClient = new QueryClient();
 
@@ -27,16 +28,18 @@ function MyApp({ Component, pageProps }: AppProps) {
               rel="stylesheet"
               href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
             />
-            <script async src={"https://www.googletagmanager.com/gtag/js?id=GTM-MPG3M856"}/>
-            <script 
-            dangerouslySetInnerHTML={{
-              __html:`window.dataLayer = window.dataLayer || [];
+            <Script async src={"https://www.googletagmanager.com/gtag/js?id=GTM-MPG3M856"} />
+            <Script
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag("config", "GTM-MPG3M856", {
                 page_path:window.location.pathname,
               })
               `,
-            }}
+              }
+              }
+              strategy="afterInteractive"
             />
           </Head>
           <Header />
